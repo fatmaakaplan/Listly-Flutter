9a0ac9dd57ae0544f46f7d800f16d412e88398c2
Fatma Kaplan <fatma.kaplan@stu.istinye.edu.tr>
2025-01-28 23:02:14 +0300

    Provider yapısı düzeltildi

--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/core/routes.dart';
-import 'package:flutter_app/screens/settings_screen.dart';
-import 'package:go_router/go_router.dart';
-import 'screens/loading_screen.dart';
-import 'screens/home_screen.dart';
-import 'screens/login_screen.dart';
-import 'screens/settings_screen.dart';
-import 'screens/profile_screen.dart';
+import 'package:provider/provider.dart';
+import 'core/routes.dart';
+import 'core/themes.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized(); // Flutter widget'larını başlat
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => ThemeProvider(),
+      child: MyApp(),
+    ),
+  );
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  bool _isDarkMode = false; // Temanın durumu
-
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'Flutter App',
-      themeMode: _isDarkMode ? ThemeMode.dark : ThemeMode.light,
-      theme: ThemeData.light(),
-      darkTheme: ThemeData.dark(),
-      routerConfig: router,
-      debugShowCheckedModeBanner: false,
-    );
-  }
-
-  void toggleTheme() {
-    setState(() {
-      _isDarkMode = !_isDarkMode;
+    return Consumer<ThemeProvider>(builder: (context, themeProvider, child) {
+      return MaterialApp.router(
+        title: 'Flutter App',
+        themeMode: themeProvider.isDark ? ThemeMode.dark : ThemeMode.light,
+        theme: lightTheme,
+        darkTheme: darkTheme,
+        routerConfig: router, // go_router yapılandırmamızı kullan
+        debugShowCheckedModeBanner: false, // Debug bandını kaldır
+      );
     });
   }
 }
-
-final router = GoRouter(
-  initialLocation: '/',
-  routes: [
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const LoadingScreen(),
-    ),
-    GoRoute(
-      path: '/home',
-      builder: (context, state) {
-        return HomeScreen(
-          onThemeChange: () {
-            final appState = context.findAncestorStateOfType<_MyAppState>();
-            appState?.toggleTheme(); // Tema değiştir
-          },
-        );
-      },
-    ),
-    GoRoute(
-      path: '/profile',
-      builder: (context, state) => const LoginScreen(),
-    ),
-    GoRoute(
-      path: '/settings',
-      builder: (context, state) => const SettingsScreen(),
-    ),
-  ],
-);
